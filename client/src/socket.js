a9da261ccb6bcc56dcf38d766b12b71d722d3ae2
Jason Hackleman <hackleman.jason@gmail.com>
2021-10-26 15:14:47 -0500

    removing redundant event handler

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,6 @@
 import io from "socket.io-client";
 import store from "./store";
 import {
-  setNewMessage,
   removeOfflineUser,
   addOnlineUser,
 } from "./store/conversations";
@@ -17,9 +16,6 @@ socket.on("connect", () => {
 
   socket.on("remove-offline-user", (id) => {
     store.dispatch(removeOfflineUser(id));
-  });
-  socket.on("new-message", (data) => {
-    store.dispatch(setNewMessage(data.message, data.sender));
   });
 });
 
