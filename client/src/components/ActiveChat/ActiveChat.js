c33905d34538af9221d1f93f13b936c3e8138980
Jason Hackleman <hackleman.jason@gmail.com>
2021-10-26 20:59:19 -0500

    undo: undoing some problematic commits

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -52,15 +52,14 @@ const ActiveChat = (props) => {
 };
 
 const mapStateToProps = (state) => {
-  const conversation = 
-    state.conversations.find(
-      (conversation) => conversation.otherUser.username === state.activeConversation
-    )
-  
-  return {
-    user: state.user,
-    conversation: {...conversation}
-  };
+    return {
+      user: state.user,
+      conversation:
+        state.conversations &&
+        state.conversations.find(
+          (conversation) => conversation.otherUser.username === state.activeConversation
+        )
+    };
 };
 
 export default connect(mapStateToProps, null)(ActiveChat);
--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 import store from "./store";
 import {
+  setNewMessage,
   removeOfflineUser,
   addOnlineUser,
 } from "./store/conversations";
@@ -16,6 +17,10 @@ socket.on("connect", () => {
 
   socket.on("remove-offline-user", (id) => {
     store.dispatch(removeOfflineUser(id));
+  });
+
+  socket.on("new-message", (data) => {
+    store.dispatch(setNewMessage(data.message, data.sender));
   });
 });
 
